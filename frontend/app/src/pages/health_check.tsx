7c0cc3c8421e8fe7753c0f1da1d8c5d14be14cea
kawabuchi-tetsuya <95535099+kawabuchi-tetsuya@users.noreply.github.com>
2025-01-23 22:04:01 +0900

    new: DockerでRails, MySQL, React環境を構築した。

---
+++ frontend/app/src/pages/health_check.tsx
@@ -0,0 +1,19 @@
+import useSWR from 'swr';
+import { fetcher } from '../utils/index';
+
+const HealthCheck: React.FC = () => {
+  const url = 'http://localhost:3000/api/v1/health_check'
+  const {data, error } = useSWR(url, fetcher)
+
+  if (error) return <div>An error has occurred.</div>
+  if (!data) return <div>Loading...</div>
+
+  return (
+    <div>
+      <h1>Health Check</h1>
+      <p>{data.message || 'Loading...'}</p>
+    </div>
+  );
+};
+
+export default HealthCheck;
