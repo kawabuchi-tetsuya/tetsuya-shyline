d59e478d77c7d3e05f22680c35f336bbfd688510
kawabuchi-tetsuya <95535099+kawabuchi-tetsuya@users.noreply.github.com>
2025-02-20 13:33:52 +0900

    feat: posts controllerにindexとshowアクションを実装した

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,32 @@
-User.create!(
-  name: 'user1',
-  screen_name: 'ユーザー1',
-  email: 'user1@example.com',
-  password: 'p@ssw0rd',
-  password_confirmation: 'p@ssw0rd',
-)
+USERS_COUNT = 2
+TOTAL_POSTS_COUNT = 30
+POST_BATCH_SIZE = 1000
+
+users = []
+USERS_COUNT.times do |i|
+  name = "ユーザー#{i + 1}"
+  uid = "user#{i + 1}"
+  email = "user#{i + 1}@example.com"
+  password = 'p@ssw0rd'
+  confirmed_at = Time.current
+
+  users << User.new(name:, uid:, email:, password:, confirmed_at:)
+end
+
+User.import users
+
+posts = []
+TOTAL_POSTS_COUNT.times do |i|
+  user_id = i % USERS_COUNT + 1
+  content = Faker::Lorem.sentence(word_count: 20)
+  status = 'published'
+
+  posts << Post.new(user_id:, content:, status:)
+
+  if posts.size >= POST_BATCH_SIZE
+    Post.import(posts)
+    posts.clear
+  end
+end
+
+Post.import(posts) unless posts.empty?
---
+++ backend/spec/requests/api/v1/posts_spec.rb
@@ -0,0 +1,64 @@
+RSpec.describe 'Api::V1::Posts', type: :request do
+  describe 'GET api/v1/posts' do
+    subject { get(api_v1_posts_path) }
+
+    before do
+      create_list(:post, 3)
+    end
+
+    context '投稿一覧ページにアクセスするとき' do
+      it 'アクセスに成功する' do
+        subject
+        res = response.parsed_body
+
+        aggregate_failures do
+          expect(response).to have_http_status(:success)
+          expect(res[0].keys).to eq ['id', 'content', 'created_at', 'from_today', 'user']
+          expect(res[0]['user'].keys).to eq ['name']
+          expect(res.length).to eq(3)
+        end
+      end
+    end
+  end
+
+  describe 'GET api/v1/posts/:id' do
+    subject { get(api_v1_post_path(post_id)) }
+
+    context '存在する投稿詳細ページにアクセスするとき' do
+      context 'ステータスが published のとき' do
+        let!(:published_post) { create(:post, status: :published) }
+        let!(:post_id) { published_post.id }
+
+        it 'アクセスに成功する' do
+          subject
+          res = response.parsed_body
+
+          aggregate_failures do
+            expect(response).to have_http_status(:success)
+            expect(res.keys).to eq ['id', 'content', 'created_at', 'from_today', 'user']
+            expect(res['user'].keys).to eq ['name']
+          end
+        end
+      end
+
+      context 'ステータスが draft のとき' do
+        let!(:draft_post) { create(:post, status: :draft) }
+        let!(:post_id) { draft_post.id }
+
+        it '404エラーが返る' do
+          subject
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+    end
+
+    context '存在しない投稿詳細ページにアクセスするとき' do
+      let!(:post_id) { 999_999_999 }
+
+      it '404エラーが返る' do
+        subject
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
+end
