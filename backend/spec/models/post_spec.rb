4de3ff3fabee733556c00d86cf7e195e5ecbc4d8
kawabuchi-tetsuya <95535099+kawabuchi-tetsuya@users.noreply.github.com>
2025-04-22 10:37:14 +0900

    test: post_specでshoulda-matchersを使うよう変更した

--- backend/spec/models/post_spec.rb
+++ backend/spec/models/post_spec.rb
@@ -8,68 +8,26 @@ RSpec.describe Post, type: :model do
   end
 
   describe 'validations' do
-    it { should define_enum_for(:status) }
-    it { should validate_presence_of(:status) }
-
     describe 'statusについて' do
-      context '規定の値であるとき' do
-        let!(:post) { create(:post) }
+      subject { build(:post) }
 
-        it '有効となる' do
-          aggregate_failures do
-            Post.statuses.each_key do |status|
-              post.status = status
-              post.valid?
-              expect(post).to be_valid
-            end
-          end
-        end
-      end
-
-      context '規定外の値であるとき' do
-        let!(:post) { build(:post, status: nil) }
-
-        it 'エラーが返る' do
-          post.valid?
-          aggregate_failures do
-            expect(post).to be_invalid
-            expect(post.errors[:status]).to include('を入力してください')
-          end
-        end
-      end
+      it { should define_enum_for(:status) }
+      it { should validate_presence_of(:status) }
+      Post.statuses.each_key {|status| it { should allow_value(status).for(:status) } }
+      it { should_not allow_value(nil).for(:status).with_message('を入力してください') }
     end
 
     describe 'content について' do
-      context 'status が published かつ content が nil のとき' do
-        let!(:post) { build(:post, content: nil) }
+      it { should allow_value('卍' * Post::CONTENT_MAX_LENGTH).for(:content) }
+      it { should_not allow_value('卍' * (Post::CONTENT_MAX_LENGTH + 1)).for(:content).with_message("は#{Post::CONTENT_MAX_LENGTH}文字以内で入力してください") }
 
-        it 'エラーが返る' do
-          post.valid?
+      context 'status が published のとき' do
+        subject { build(:post, status: :published, content: nil) }
+
+        it 'content が必須である' do
           aggregate_failures do
-            expect(post).to be_invalid
-            expect(post.errors[:content]).to include('を入力してください')
-          end
-        end
-      end
-      context "#{Post::CONTENT_MAX_LENGTH} 文字のとき" do
-        let!(:post) { create(:post) }
-
-        it '有効となる' do
-          post.content = '卍' * Post::CONTENT_MAX_LENGTH
-          post.valid?
-          expect(post).to be_valid
-        end
-      end
-
-      context "#{Post::CONTENT_MAX_LENGTH + 1} 文字のとき" do
-        let!(:post) { build(:post) }
-
-        it '無効となる' do
-          post.content = '卍' * (Post::CONTENT_MAX_LENGTH + 1)
-          post.valid?
-          aggregate_failures do
-            expect(post).to be_invalid
-            expect(post.errors[:content]).to include("は#{Post::CONTENT_MAX_LENGTH}文字以内で入力してください")
+            expect(subject).to be_invalid
+            expect(subject.errors[:content]).to include('を入力してください')
           end
         end
       end
@@ -79,13 +37,12 @@ RSpec.describe Post, type: :model do
       context '同じユーザーが2つ以上同時に保存しようとしたとき' do
         let!(:user) { create(:user) }
         let!(:post) { create(:post, user:, status: :unsaved) }
-        let!(:new_post) { build(:post, user:, status: :unsaved) }
+        subject { build(:post, user:, status: :unsaved) }
 
         it 'エラーが返る' do
-          new_post.valid?
           aggregate_failures do
-            expect(new_post).to be_invalid
-            expect(new_post.errors[:base]).to include('未保存の投稿は複数保有できません')
+            expect(subject).to be_invalid
+            expect(subject.errors[:base]).to include('未保存の投稿は複数保有できません')
           end
         end
       end
